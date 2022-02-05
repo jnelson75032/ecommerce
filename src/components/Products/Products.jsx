8e9b98c65caf0d692a6d5ae9667bb23c9def1f8d
John Nelson <jrnelson654@gmail.com>
2022-02-05 16:30:49 -0600

    Adds commerce.js

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,12 +4,7 @@ import { Grid } from '@material-ui/core';
 import Product from './Product/Product';
 import useStyles from './styles';
 
-const products = [
-    { id: 1, name: 'Shoes', description: 'Running Shoes', price: '$5' },
-    { id: 2, name: 'Mac Book', description: 'Apple Mac Book', price: '$10' },
-];
-
-const Products = () => {
+const Products = ({ products }) => {
     const classes = useStyles();
 
     return (
---
+++ src/lib/commerce.js
@@ -0,0 +1,3 @@
+import Commerce from '@chec/commerce.js';
+
+export const commerce = new Commerce('pk_test_39487e0fcac34c7b6a6228349dd006774a7a9710b2cda', true);
